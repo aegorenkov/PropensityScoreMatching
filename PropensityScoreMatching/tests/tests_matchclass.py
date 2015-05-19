3abbdda063c999dfd204c52385fd49f23d1ae3e8
Alexander Egorenkov <aegorenk@gmail.com>
2015-05-19 15:09:27 -0400

    Rewrite matching tests

--- PropensityScoreMatching/tests/tests_matchclass.py
+++ PropensityScoreMatching/tests/tests_matchclass.py
@@ -11,9 +11,9 @@ import pandas as pd
 import numpy as np
 import os
 LOCAL_DIR = os.path.dirname(__file__)
-FILENAMES = [r'results/nsw_all_random1_pscoresimple.csv',
-             r'results/nsw_all_random2_pscoresimple.csv',
-             r'results/nsw_all_random3_pscoresimple.csv']
+FILENAMES = [os.path.join('results', 'nsw_all_random1_pscoresimple.csv'),
+             os.path.join('results', 'nsw_all_random2_pscoresimple.csv'),
+             os.path.join('results', 'nsw_all_random3_pscoresimple.csv')]
 FILEPATHS = [os.path.join(LOCAL_DIR, name) for name in FILENAMES]
 
 DATASET1 = pd.read_csv(FILEPATHS[0])
@@ -27,23 +27,48 @@ class MatchClass(unittest.TestCase):
         match = PSM.Match()
         self.assertEqual(match.match_type, 'neighbor')
 
-    def test_set1_matches_in_order(self):
-        testdata = DATASET1.sort(columns="_id")
+    def test_set1_idlist_is_same_length_as_n(self):
+        testdata = DATASET1.sort(columns="_id", )
         match = PSM.Match()
-        id_list = match.match(testdata)
-        self.assertTrue(all(id_list == DATASET1["_n1"]))
+        id_list = match.match(testdata["Treated"], testdata["_pscore"])
+        self.assertTrue(len(id_list) == len(DATASET1["_n1"]),
+                        msg="List of matches has incorrect length")
+
+    def test_set1_matches_in_order(self):
+        testdata = DATASET1
+        match = PSM.Match()
+        id_list = match.match(testdata["Treated"], testdata["_pscore"])
+        test_list, true_list = testdata["_id"][id_list], testdata["_n1"]
+        #Raise assertionError if id_list cannot match the order if id and n1
+        np.testing.assert_array_equal(test_list, true_list)
+        #Explicitly test matching without nan values
+        test_list = test_list[np.isfinite(test_list)]
+        true_list = true_list[np.isfinite(true_list)]
+        self.assertTrue(np.array_equal(test_list, true_list))
 
     def test_set2_matches_in_order(self):
-        testdata = DATASET2.sort(columns="_id")
+        testdata = DATASET2
         match = PSM.Match()
-        id_list = match.match(testdata)
-        self.assertTrue(all(id_list == DATASET2["_n1"]))
+        id_list = match.match(testdata["Treated"], testdata["_pscore"])
+        test_list, true_list = testdata["_id"][id_list], testdata["_n1"]
+        #Raise assertionError if id_list cannot match the order if id and n1
+        np.testing.assert_array_equal(test_list, true_list)
+        #Explicitly test matching without nan values
+        test_list = test_list[np.isfinite(test_list)]
+        true_list = true_list[np.isfinite(true_list)]
+        self.assertTrue(np.array_equal(test_list, true_list))
 
     def test_set3_matches_in_order(self):
-        testdata = DATASET3.sort(columns="_id")
+        testdata = DATASET3
         match = PSM.Match()
-        id_list = match.match(testdata)
-        self.assertTrue(all(id_list == DATASET3["_n1"]))
+        id_list = match.match(testdata["Treated"], testdata["_pscore"])
+        test_list, true_list = testdata["_id"][id_list], testdata["_n1"]
+        #Raise assertionError if id_list cannot match the order if id and n1
+        np.testing.assert_array_equal(test_list, true_list)
+        #Explicitly test matching without nan values
+        test_list = test_list[np.isfinite(test_list)]
+        true_list = true_list[np.isfinite(true_list)]
+        self.assertTrue(np.array_equal(test_list, true_list))
 
 class PropensityScoreMatchingClass(unittest.TestCase):
     #We don't define setUp because we will need to change parameters of the
