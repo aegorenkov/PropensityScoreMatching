cd01a79a025acf8c1745a3627eb19c495f43dca5
Alexander Egorenkov <aegorenk@gmail.com>
2015-05-19 11:54:02 -0400

    Setup basic tests

--- PropensityScoreMatching/__init__.py
+++ PropensityScoreMatching/__init__.py
@@ -6,11 +6,14 @@ Created on Mon May 18 15:09:03 2015
 """
 class Match(object):
     '''
-    Perform matching algorithm on input data and return a list list of indicies
+    Perform matching algorithm on input data and return a list of indicies
     corresponding to matches.
     '''
     def __init__(self, match_type='neighbor'):
         self.match_type = match_type
+        
+    def match(self, covariates):
+        pass
 
 class PropensityScoreMatching(object):
     '''
@@ -19,6 +22,12 @@ class PropensityScoreMatching(object):
     '''
     def __init__(self, model='logit'):
         self.model = model
+        
+    def fit(self, treated, design_matrix):
+        '''
+        Run logit or probit and return propensity score column
+        '''
+        pass
 
 class MahalanobisMatching(object):
     '''
--- PropensityScoreMatching/tests/tests_matchclass.py
+++ PropensityScoreMatching/tests/tests_matchclass.py
@@ -6,18 +6,78 @@ Created on Mon May 18 14:30:15 2015
 """
 
 import unittest
-import PropensityScoreMatching
+import PropensityScoreMatching as PSM
+import pandas as pd
+import numpy as np
+import os
+LOCAL_DIR = os.path.dirname(__file__)
+FILENAMES = [r'results/nsw_all_random1_pscoresimple.csv',
+             r'results/nsw_all_random2_pscoresimple.csv',
+             r'results/nsw_all_random3_pscoresimple.csv']
+FILEPATHS = [os.path.join(LOCAL_DIR, name) for name in FILENAMES]
+SET1 = pd.read_csv(FILEPATHS[0])
+SET2 = pd.read_csv(FILEPATHS[1])
+SET3 = pd.read_csv(FILEPATHS[2])
 
 class MatchClass(unittest.TestCase):
     def test_match_can_initialize(self):
-        match = PropensityScoreMatching.Match()
+        match = PSM.Match()
         self.assertEqual(match.match_type, 'neighbor')
 
-class TestPropensityScoreMatchingClass(unittest.TestCase):
-    pass
+    def test_set1_matches_in_order(self):
+        testdata = SET1.sort(columns="_id")
+        match = PSM.Match()
+        id_list = match.match(testdata)
+        self.assertTrue(all(id_list == SET1["_n1"]))
+
+    def test_set2_matches_in_order(self):
+        testdata = SET2.sort(columns="_id")
+        match = PSM.Match()
+        id_list = match.match(testdata)
+        self.assertTrue(all(id_list == SET2["_n1"]))
+
+    def test_set3_matches_in_order(self):
+        testdata = SET3.sort(columns="_id")
+        match = PSM.Match()
+        id_list = match.match(testdata)
+        self.assertTrue(all(id_list == SET3["_n1"]))
+
+class PropensityScoreMatchingClass(unittest.TestCase):
+    #We don't define setUp because we will need to change parameters of the 
+    #psm instance
+    def test_PSM_can_initialize(self):
+        psm = PSM.PropensityScoreMatching()
+        self.assertEqual(psm.model, 'logit')
+    
+    def test_set1_pscores_should_equal_data_pscores(self):
+        treated = SET1.Treated
+        design_matrix = SET1.Age
+        psm = PSM.PropensityScoreMatching()
+        pscore_fit = psm.fit(treated, design_matrix)
+        pscore_actual = SET1._pscore
+        SS_diff = np.sum((pscore_fit-pscore_actual)**2)
+        self.assertAlmostEqual(SS_diff, 0)
+    
+    def test_set2_pscores_should_equal_data_pscores(self):
+        treated = SET2.Treated
+        design_matrix = SET2.Age
+        psm = PSM.PropensityScoreMatching()
+        pscore_fit = psm.fit(treated, design_matrix)
+        pscore_actual = SET2._pscore
+        SS_diff = np.sum((pscore_fit-pscore_actual)**2)
+        self.assertAlmostEqual(SS_diff, 0)
+    
+    def test_set3_pscores_should_equal_data_pscores(self):
+        treated = SET3.Treated
+        design_matrix = SET3.Age
+        psm = PSM.PropensityScoreMatching()
+        pscore_fit = psm.fit(treated, design_matrix)
+        pscore_actual = SET3._pscore
+        SS_diff = np.sum((pscore_fit-pscore_actual)**2)
+        self.assertAlmostEqual(SS_diff, 0)
 
 class TestMahalanobisMatchingClass(unittest.TestCase):
     pass
 
-#if __name__ == '__main__':
-    #unittest.main()
+if __name__ == '__main__':
+    unittest.main()
